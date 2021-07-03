a66271efaf16b727e96d88684545073fced4d129
Giannandrea <34889642+Giannandrea-Vicidomini@users.noreply.github.com>
2021-07-03 20:04:42 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,19 @@ import 'package:first_app/Screens/FormSubmit.dart';
 import 'package:first_app/Screens/LoadingPage.dart';
 import 'package:first_app/Screens/ResultsPage.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:window_size/window_size.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle("Digital Forensics Framework");
+    //setWindowMinSize(Size(1080, 720));
+    //setWindowMaxSize(Size(1200, 1000));
+    setWindowFrame(Rect.fromLTRB(100, 100, 1200, 720));
+  }
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
